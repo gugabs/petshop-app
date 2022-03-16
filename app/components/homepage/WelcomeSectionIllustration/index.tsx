171a02edf9854bcfdcfa0ed193457e67e509f055
Guga <contato.gustavobarbosa@outlook.com>
2022-03-15 22:05:21 -0300

    homepage welcome section

---
+++ app/components/homepage/WelcomeSectionIllustration/index.tsx
@@ -0,0 +1,5 @@
+import DogWalking from '~/assets/images/illustrations/dog-walking.svg';
+
+export default function WelcomeSectionIllustration() {
+  return <img src={DogWalking} alt="" />;
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,55 @@
+import { Link, LinksFunction } from 'remix';
+
+import globalStyle from '~/styles/index.css';
+import componentStyle from '~/styles/homepage/index.css';
+
+import WelcomeSectionIllustration from '~/components/homepage/WelcomeSectionIllustration/index';
+
+import {
+  WelcomeSectionDivider,
+  links as WelcomeSectionDividerLinks,
+} from '~/components/homepage/WelcomeSectionDivider';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStyle,
+    },
+    {
+      rel: 'stylesheet',
+      href: componentStyle,
+    },
+    ...WelcomeSectionDividerLinks(),
+  ];
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="container">
+        <section className="section-welcome">
+          <div className="welcome-infos">
+            <h1>Petshop</h1>
+            <h2>Faça o agendamento de um de nossos serviços agora mesmo!</h2>
+            <div className="welcome-btn-container">
+              <Link to={'/createSchedule'}>
+                <div className="welcome-btn-wrapper">
+                  <span>Agendar</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="welcome-divider"></div>
+          <div className="welcome-illustration">
+            <WelcomeSectionIllustration />
+          </div>
+          <div className="welcome-section-divider">
+            <WelcomeSectionDivider />
+          </div>
+        </section>
+        <section className="section-schedules"></section>
+      </div>
+    </>
   );
 }
